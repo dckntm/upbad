b61439d6f01406f80492f0ec02735ffa7e5a9d41
Katun Ilya <katun.ilya@gmail.com>
2022-03-21 20:04:49 +0300

    Fix tests
    * Remove odd tests
    * Add mock test for __version__
    * Add __init__.py to fix VS Code pytest discovery

--- tests/sample_test.py
+++
@@ -1,6 +0,0 @@
-def sample_func():
-    return True
-
-
-def test_sample():
-    assert sample_func() is True
---
+++ tests/test_upbad.py
@@ -0,0 +1,6 @@
+from upbad import __version__
+
+
+# TODO remove this example test in future
+def test_version():
+    assert __version__ == "0.0.1"
